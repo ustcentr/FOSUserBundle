20e1047f4029f183f1ad2a12188d66bbe3aac8dd
Vladimir <ustcentr@gmail.com>
2021-09-02 12:22:47 +0300

    Fix UserListing

--- Doctrine/UserListener.php
+++ Doctrine/UserListener.php
@@ -12,7 +12,7 @@
 namespace FOS\UserBundle\Doctrine;
 
 use Doctrine\Common\EventSubscriber;
-use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs as LifecycleEventArgs;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ORM\EntityManager;
